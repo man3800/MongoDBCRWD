3e341cd1a9ee88cd2e84fc3c3f1a8c70633f1961
admin <admin@HNKDEV-NOTEBOOK>
2022-03-22 17:46:59 +0900

    without "BsonId" first test

---
+++ MongoDBCRWD/Models/People.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDBCRWD.Models
+{
+    public class People
+    {
+        //[BsonId]
+        //[BsonRepresentation(BsonType.ObjectId)]
+        //public string id { get; set; }
+
+        [BsonElement("name")]
+        public string name { get; set; }
+        
+        [BsonElement("age")]
+        public int age { get; set; }
+
+    }
+}
--- MongoDBCRWD/Program.cs
+++ MongoDBCRWD/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using MongoDBCRWD.Models;
+using System;
 
 namespace MongoDBCRWD
 {
@@ -6,7 +8,14 @@ namespace MongoDBCRWD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("school");
+
+            var peopleDB = database.GetCollection<People>("people");
+
+            var people = new People() { name = "홍길이", age = 12 };
+            peopleDB.InsertOne(people);
+
         }
     }
 }
