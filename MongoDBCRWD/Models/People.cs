3dd6ff3a1bfd4131afaccd22392981f7bde1deb6
admin <admin@HNKDEV-NOTEBOOK>
2022-03-23 09:03:36 +0900

    finish

--- MongoDBCRWD/Models/People.cs
+++ MongoDBCRWD/Models/People.cs
@@ -7,10 +7,10 @@ using MongoDB.Bson.Serialization.Attributes;
 namespace MongoDBCRWD.Models
 {
     public class People
-    {
-        //[BsonId]
-        //[BsonRepresentation(BsonType.ObjectId)]
-        //public string id { get; set; }
+    {       
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { get; set; }
 
         [BsonElement("name")]
         public string name { get; set; }
--- MongoDBCRWD/Program.cs
+++ MongoDBCRWD/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MongoDB.Driver;
 using MongoDBCRWD.Models;
 using System;
+using System.Collections.Generic;
 
 namespace MongoDBCRWD
 {
@@ -13,9 +14,40 @@ namespace MongoDBCRWD
 
             var peopleDB = database.GetCollection<People>("people");
 
-            var people = new People() { name = "홍길이", age = 12 };
-            peopleDB.InsertOne(people);
+            var people = new People() { id = "62398f98b83e571add593289", name = "길용이 잘하고 있나?", age = 14 };
+            //insert
+            //peopleDB.InsertOne(people);
 
+            //select
+            //List<People> lst = peopleDB.Find(d => true).ToList();
+
+            //update
+            peopleDB.ReplaceOne(d => d.id == "62398f98b83e571add593289", people);
+
+            //delete
+            //peopleDB.DeleteOne(d => d.id == "62398f98b83e571add593289");
+
+            /*
+             * vs project 마다 mongodb.driver설치
+                install-package mongdb.driver   
+             * pc MongoDB설치
+                환경변수 path add "C:\Program Files\MongoDB\Server\5.0\bin"
+             * dos
+                C:\Users\admin>mongo             
+                MongoDB Enterprise > show dbs
+                admin    0.000GB
+                config   0.000GB
+                local    0.000GB
+                school   0.000GB
+                startup  0.000GB
+                MongoDB Enterprise > use school
+                switched to db school
+                MongoDB Enterprise > db
+                school
+                MongoDB Enterprise > db.people.find()
+                { "_id" : ObjectId("62398f98b83e571add593289"), "name" : "길용이 잘하고 있나?", "age" : 14 }
+                MongoDB Enterprise >
+             */
         }
     }
 }
