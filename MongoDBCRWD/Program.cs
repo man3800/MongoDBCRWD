20ff917f3f15c6e592e3f1e1bc8f9f7fd7ea98e4
man3801 <man3801@gmail.com>
2022-03-23 10:05:50 +0900

    PC Chage Test

--- MongoDBCRWD/Program.cs
+++ MongoDBCRWD/Program.cs
@@ -9,12 +9,14 @@ namespace MongoDBCRWD
     {
         static void Main(string[] args)
         {
-            var client = new MongoClient("mongodb://localhost:27017");
+            var client = new MongoClient("mongodb://172.1.50.51:27017");
             var database = client.GetDatabase("school");
 
             var peopleDB = database.GetCollection<People>("people");
 
-            var people = new People() { id = "62398f98b83e571add593289", name = "길용이 잘하고 있나?", age = 14 };
+            //var people = new People() { name = "홍길동이 어디고?", age = 28 };
+            var people = new People() { id = "623a7178f47ef0eb3042dd20", name = "홍길동이 어디고? 니 뒤에 있다.", age = 38 };
+
             //insert
             //peopleDB.InsertOne(people);
 
@@ -22,7 +24,7 @@ namespace MongoDBCRWD
             //List<People> lst = peopleDB.Find(d => true).ToList();
 
             //update
-            peopleDB.ReplaceOne(d => d.id == "62398f98b83e571add593289", people);
+            peopleDB.ReplaceOne(d => d.id == "623a7178f47ef0eb3042dd20", people);
 
             //delete
             //peopleDB.DeleteOne(d => d.id == "62398f98b83e571add593289");
